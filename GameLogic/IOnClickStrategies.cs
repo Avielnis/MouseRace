301f48aa3618ff76e4cb505836d64820151a09a1
Avielnis <avielnis222@gmail.com>
2024-06-30 12:36:05 +0300

    Added Sound Effects

--- GameLogic/IOnClickStrategies.cs
+++ GameLogic/IOnClickStrategies.cs
@@ -1,16 +1,27 @@
-﻿namespace GameLogic
+﻿using System.Media;
+
+namespace GameLogic
 {
     public interface IOnClickStrategies
     {
         void Invoke(Element element);
+        void PlaySound();
+
     }
 
     public class CollectStrategy : IOnClickStrategies
     {
         public void Invoke(Element element)
         {
+            PlaySound();
             GameEngine.Instance.AddCollected(element);
             element.Parent.Controls.Remove(element);
+        }
+
+        public void PlaySound()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\aviel\IDC\MyProjects\Prismm\MouseRace\GameLogic\Resources\CollectSound.wav");
+            player.Play();
         }
     }
 
@@ -18,7 +29,14 @@
     {
         public void Invoke(Element element)
         {
+            PlaySound();
             GameEngine.Instance.LoseGame();
+        }
+
+        public void PlaySound()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\aviel\IDC\MyProjects\Prismm\MouseRace\GameLogic\Resources\AvoidSound.wav");
+            player.Play();
         }
     }
 }
